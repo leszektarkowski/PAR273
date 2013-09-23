a761fce79bf09c2e189084ccf7c477ad5c77aa2c
Potencjostat <potencjostat@potencjostat-desktop.(none)>
2013-09-23 12:46:26 +0200

    Working version 0.1

--- server/server.py
+++ server/server.py
@@ -1,5 +1,14 @@
 import gpib
 
+class PARWriteError(Exception):
+    pass
+
+class PARReadError(Exception):
+    pass
+
+class PARCellWorking(Exception):
+    pass
+
 class Poll:
     COMMAND_DONE  = 1
     COMMAND_ERROR = 2
@@ -10,27 +19,58 @@ class Poll:
     OUTPUT_READY  = 128
 
 class PAR(object):
-	def __init__(self, addres):
-		self.dev = gpib.dev(*addres)
-		write('DD 13')
-		write('TYPE Succesfull init"')
+    def __init__(self, addres):
+        self.dev = gpib.dev(*addres)
+        self.write('DD 13')
+        self.write('TYPE Succesfull init"')
 
-	def write(cmd):		
-		while(true):
-			status = gpib.serial_poll(self.dev)
-			if (status & Poll.COMMAND_DONE != 0)
-				break
-		gpib.write(self.dev, cmd)
+    def write(self, cmd):		
+        while True:            
+            status = ord(gpib.serial_poll(self.dev))
+            #print '.',
+            if status & Poll.COMMAND_DONE != 0:
+                break
+            elif status & Poll.OUTPUT_READY != 0:
+                raise PARWriteError("Data is ready, can't write")
+        gpib.write(self.dev, cmd)
 
-	def read():
-		while(true):
-			status = gpib.serial_poll(self.dev)
-			if (status & Pool.OUTPUT_READY != 0)
-				break
-		return gpib.read(self.dev)
+    def read(self):
+        while True:            
+            status = ord(gpib.serial_poll(self.dev))
+            #print ':', status,
+            if status & Poll.OUTPUT_READY != 0:
+                break
+            elif status & Poll.COMMAND_DONE != 0:
+                raise PARReadError("Nothing to read")
+        return gpib.read(self.dev, 1024)
+
+    def ask(self, cmd):
+        self.write(cmd)
+        return self.read()
+
+    def wait_for_relay(self):        
+        cell_hw_switch = int(p.ask("CS").split()[0])
+        cell_relay = int(p.ask("CELL").split()[0])
+        if cell_hw_switch and cell_relay:
+            raise PARCellWorking("Both cell switches enabled")
+        elif cell_hw_switch == False and cell_relay:
+            raise PARCellWorking("Cell relay in waiting status...")
+        elif cell_hw_switch == True and cell_relay == False:
+            raise PARCellWorking("Previous measurement not finished!")
+        elif cell_hw_switch == False and cell_relay == False:
+            for i in range(100):
+                print "Press Cell Switch!"
+                import time
+                time.sleep(0.2)
+                cell_hw_switch = int(p.ask("CS").split()[0])
+                if cell_hw_switch:
+                    p.write("CELL 1")
+                    print "Measurement started"
+                    return True
+            return False
 
 
-
+p = PAR( (0,14) )
 
 
 
